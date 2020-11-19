53a8f911abb87ef20e3489af797475c2161c2c23
Joseph <josephajurney@gmail.com>
2020-11-19 14:23:18 -0500

    created player class

---
+++ DungeonsAndDatabases.Data/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Data
+{
+    public class Player
+    {
+        [Key]
+        public Guid PlayerID { get; set; }
+        [Required]
+        public string PlayerName { get; set; }
+
+
+        //public virtual List<Character> Characters { get; set; } = new List<Character>();
+        //public virtual List<Campaign> Campaigns { get; set; } = new List<Campaign>();
+        //public virtual List<Membership> CharacterCampaign { get; set; } = new List<Membership>();
+    }
+}

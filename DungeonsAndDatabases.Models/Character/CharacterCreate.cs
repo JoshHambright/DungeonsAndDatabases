8de22140c2ebefb8c41ef3b2bfcc194c285a34c0
samuel ayorinde <ryuyoko@hotmail.com>
2020-11-19 14:23:29 -0600

    Character Create reupload

--- DungeonsAndDatabases.Models/Character/CharacterCreate.cs
+++ DungeonsAndDatabases.Models/Character/CharacterCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace DungeonsAndDatabases.Models.Character
 {
     class CharacterCreate
     {
+        [Required]
+        public string CharacterName { get; set; }
+        [Required]
+        public string Race { get; set; }
+        [Required]
+        public string Class { get; set; }
+        [Required]
+        public string Level { get; set; }
     }
 }

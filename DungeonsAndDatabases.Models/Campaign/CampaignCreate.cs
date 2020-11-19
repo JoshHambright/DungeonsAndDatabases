2e024f10632994a5796ad7bb54f4a73658e0002f
JoshHambright <josh.hambright@gmail.com>
2020-11-19 14:58:33 -0500

    bleepbloop required

--- DungeonsAndDatabases.Models/Campaign/CampaignCreate.cs
+++ DungeonsAndDatabases.Models/Campaign/CampaignCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace DungeonsAndDatabases.Models.Campaign
     public class CampaignCreate
     {
         //Model for creating a campaign
+        [Required]
         public string CampaignName { get; set; }
+        [Required]
         public string GameSystem { get; set; }
+        [Required]
         public Guid DmGuid { get; set; }
 
     }

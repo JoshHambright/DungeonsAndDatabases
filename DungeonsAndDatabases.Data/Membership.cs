7f11bc9efe0d0d67bad94ada61ca9143c4d9cfa7
JoshHambright <josh.hambright@gmail.com>
2020-11-19 14:30:09 -0500

    Membership data class

---
+++ DungeonsAndDatabases.Data/Membership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Data
+{
+    public class Membership
+    {
+        [Key, Column(Order = 0)]
+        public int CampaignId { get; set; }
+        //public virtual Campaign Campaign { get; set; }
+        [Key, Column(Order = 1)]
+        public int CharacterId { get; set; }
+        //public virtual Character Character { get; set; }
+    }
+}

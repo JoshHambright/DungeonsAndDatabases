162849fb76e9df8c0a8c6b16c81d5ea2865dad76
Joseph <josephajurney@gmail.com>
2020-11-19 14:51:51 -0500

    created player models

---
+++ DungeonsAndDatabases.Models/Player/PlayerListItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Models.Player
+{
+    public class PlayerListItem
+    {
+        public string PlayerName { get; set; }
+    }
+}

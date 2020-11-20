81b616d010c1138821569efe9016337ca79e9ed0
JoshHambright <josh.hambright@gmail.com>
2020-11-20 10:05:57 -0500

    Campaign Controller

---
+++ DungeonsAndDatabases.Services/CampaignService.cs
@@ -0,0 +1,25 @@
+﻿using DungeonsAndDatabases.Models.Campaign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Services
+{
+    public class CampaignService
+    {
+        //GUID
+        private readonly Guid _userId;
+
+        public CampaignService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCampaign(CampaignCreate model)
+        {
+            return
+        }
+    }
+}

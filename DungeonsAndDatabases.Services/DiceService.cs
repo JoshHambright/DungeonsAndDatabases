3c489f9c51b079f6998548796886327991536dad
JoshHambright <josh.hambright@gmail.com>
2020-11-24 22:41:33 -0500

    RNG Service and Dice Controller endpoint

---
+++ DungeonsAndDatabases.Services/DiceService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Services
+{
+    public class DiceService
+    {
+        private static readonly RNGCryptoServiceProvider _randy = new RNGCryptoServiceProvider();
+        public int GetDiceRoll(int dicesides)
+        {
+            int diceRoll = 0;
+            while (diceRoll < 1)
+            {
+
+                byte[] randy = new byte[1];
+
+                _randy.GetBytes(randy);
+
+                double asciiValueOfRandomCharacter = Convert.ToDouble(randy[0]);
+
+                double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+                double randomRoll = Math.Floor(multiplier * (dicesides+1));
+                diceRoll = Convert.ToInt32(randomRoll);
+            }
+            return diceRoll;
+        }
+
+
+    }
+}
---
+++ DungeonsAndDatabases.WebAPI/Controllers/DiceController.cs
@@ -0,0 +1,28 @@
+﻿using DungeonsAndDatabases.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DungeonsAndDatabases.WebAPI.Controllers
+{
+    public class DiceController : ApiController
+    {
+        private DiceService CreateDiceService()
+        {
+            var diceService = new DiceService();
+            return diceService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult RollDice(int D)
+        {
+            DiceService dice = CreateDiceService();
+            var diceRoll = dice.GetDiceRoll(D);
+
+            return Ok(diceRoll);
+        }
+    }
+}

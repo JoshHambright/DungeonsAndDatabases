17b82c13efe7effd6c71f30ed90a8a8279940304
JoshHambright <josh.hambright@gmail.com>
2020-11-29 16:44:02 -0500

    dice models, roll logic for multiple dice

---
+++ DungeonsAndDatabases.Models/DiceModels/DiceResults.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Models.DiceModels
+{
+    public class DiceResults
+    {
+        public List<DiceSingle> Rolls { get; set; } = new List<DiceSingle>();
+        public int Total { get
+            {
+                int total = 0;
+                foreach (var roll in Rolls)
+                    total += roll.Result;
+                return total;
+            } 
+        }
+        public int Advantage { get
+            {
+                int highest = Rolls.Max(roll => roll.Result);
+                return highest;
+            } 
+        }
+        public int Disadvantage { get
+            {
+                int lowest = Rolls.Min(roll => roll.Result);
+                return lowest;
+            } 
+        }
+    }
+}
--- DungeonsAndDatabases.Services/DiceService.cs
+++ DungeonsAndDatabases.Services/DiceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DungeonsAndDatabases.Models.DiceModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -24,10 +25,39 @@ namespace DungeonsAndDatabases.Services
 
                 double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
 
-                double randomRoll = Math.Floor(multiplier * (dicesides+1));
+                double randomRoll = Math.Floor(multiplier * (dicesides + 1));
                 diceRoll = Convert.ToInt32(randomRoll);
             }
             return diceRoll;
+        }
+        public DiceResults GetMultipleDiceRoll(int dicesides, int n)
+        {
+            var rolls = new DiceResults();
+            int looper = 0;
+            while (looper != n)
+            {
+                var roll = new DiceSingle();
+                int diceRoll = 0;
+                roll.DieType = $"D{dicesides}";
+                while (diceRoll < 1)
+                {
+
+                    byte[] randy = new byte[1];
+
+                    _randy.GetBytes(randy);
+
+                    double asciiValueOfRandomCharacter = Convert.ToDouble(randy[0]);
+
+                    double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+                    double randomRoll = Math.Floor(multiplier * (dicesides + 1));
+                    diceRoll = Convert.ToInt32(randomRoll);
+                }
+                roll.Result = diceRoll;
+                rolls.Rolls.Add(roll);
+                looper++;
+            }
+            return rolls;
         }
 
 

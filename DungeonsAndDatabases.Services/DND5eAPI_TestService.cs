07e09ff15580f4f90770b0b9cc8edac506bef783
JoshHambright <josh.hambright@gmail.com>
2020-12-01 09:04:28 -0500

    Service and Controller to test getting race

---
+++ DungeonsAndDatabases.Services/DND5eAPI_TestService.cs
@@ -0,0 +1,29 @@
+﻿using DungeonsAndDatabases.Models.DND5EAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Services
+{
+    public class DND5eAPI_TestService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string _baseUrl = "https://www.dnd5eapi.co/api/";
+        private readonly string _classes = "classes/";
+        private readonly string _races = "races/";
+
+        public async Task<Race> GetRaceFromAPIAsync(string race)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(_baseUrl + _races + race);
+            if (response.IsSuccessStatusCode)
+            {
+                Race result = await response.Content.ReadAsAsync<Race>();
+                return result;
+            }
+            return null;
+        }
+    }
+}

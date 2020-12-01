37596c1a9afe754d351e85b15b2253d68b9527d2
Joseph <josephajurney@gmail.com>
2020-12-01 17:08:02 -0500

    Equipment models

--- DungeonsAndDatabases.Data/Equipment.cs
+++ DungeonsAndDatabases.Data/Equipment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
@@ -12,10 +13,13 @@ namespace DungeonsAndDatabases.Data
     public class Equipment
     {
         [Key]
+        public int ID { get; set; }
         public string Name { get; set; } 
-        public string Index { get; set; }
-        public string Desc { get; set; }
+        public string Notes { get; set; }
+        public virtual Character Character { get; set; }
         public EquipmentType EquipmentType { get; set; }
-        
+
+        [ForeignKey(nameof(Character))]
+        public int CharacterID { get; set; }
     }
 }

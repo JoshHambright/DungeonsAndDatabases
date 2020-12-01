7296eccc6d4886f91c6669d65b9b653548d7dc12
samuel ayorinde <ryuyoko@hotmail.com>
2020-12-01 16:13:55 -0600

    equipment and character data class updates

--- DungeonsAndDatabases.Data/Equipment.cs
+++ DungeonsAndDatabases.Data/Equipment.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
@@ -12,10 +13,15 @@ namespace DungeonsAndDatabases.Data
     public class Equipment
     {
         [Key]
+        public int ID { get; set; }
+        [Required]
         public string Name { get; set; } 
         public string Index { get; set; }
         public string Desc { get; set; }
         public EquipmentType EquipmentType { get; set; }
+        [ForeignKey(nameof(Character))]
+        public int CharacterID { get; set; }
+        public virtual Character Character { get; set; }
         
     }
 }

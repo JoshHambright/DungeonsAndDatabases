0a2d9ee8538cdbd6d9a0bd9c729851b5db25729e
Joseph <josephajurney@gmail.com>
2020-12-01 10:22:47 -0500

    equipment models and created equipment service and equipment controller

---
+++ DungeonsAndDatabases.Models/EquipmentModels/EquipmentMagicItems.cs
@@ -0,0 +1,16 @@
+﻿using DungeonsAndDatabases.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonsAndDatabases.Models.EquipmentModels
+{
+    public class EquipmentMagicItems 
+    {
+        public string Name { get; set; }
+        public string Index { get; set; }
+        public string Desc { get; set; }
+    }
+}

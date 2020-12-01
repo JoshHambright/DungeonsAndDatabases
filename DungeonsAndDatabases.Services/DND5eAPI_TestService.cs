44310f22d5dca861b79afe2e662d93e00a1ad40d
JoshHambright <josh.hambright@gmail.com>
2020-12-01 10:58:01 -0500

    comment out ununsed test stuff

--- DungeonsAndDatabases.Services/DND5eAPI_TestService.cs
+++ DungeonsAndDatabases.Services/DND5eAPI_TestService.cs
@@ -10,42 +10,42 @@ namespace DungeonsAndDatabases.Services
 {
     public class DND5eAPI_TestService
     {
-        private readonly HttpClient _httpClient = new HttpClient();
-        private readonly string _baseUrl = "https://www.dnd5eapi.co/api/";
-        private readonly string _dnd5eAPI = "https://www.dnd5eapi.co";
-        private readonly string _classes = "classes/";
-        private readonly string _races = "races/";
+    //    private readonly HttpClient _httpClient = new HttpClient();
+    //    private readonly string _baseUrl = "https://www.dnd5eapi.co/api/";
+    //    private readonly string _dnd5eAPI = "https://www.dnd5eapi.co";
+    //    private readonly string _classes = "classes/";
+    //    private readonly string _races = "races/";
 
-        public async Task<Race> GetRaceFromAPIAsync(string race)
-        {
-            HttpResponseMessage response = await _httpClient.GetAsync(_baseUrl + _races + race);
-            if (response.IsSuccessStatusCode)
-            {
-                Race result = await response.Content.ReadAsAsync<Race>();
-                return result;
-            }
-            return null;
-        }
-        public async Task<Race_Short> GetShortRaceFromAPIAsync(string race)
-        {
-            HttpResponseMessage response = await _httpClient.GetAsync(_baseUrl + _races + race);
-            if (response.IsSuccessStatusCode)
-            {
-                Race_Short result = await response.Content.ReadAsAsync<Race_Short>();
-                return result;
-            }
-            return null;
-        }
+    //    public async Task<Race> GetRaceFromAPIAsync(string race)
+    //    {
+    //        HttpResponseMessage response = await _httpClient.GetAsync(_baseUrl + _races + race);
+    //        if (response.IsSuccessStatusCode)
+    //        {
+    //            Race result = await response.Content.ReadAsAsync<Race>();
+    //            return result;
+    //        }
+    //        return null;
+    //    }
+    //    public async Task<Race_Short> GetShortRaceFromAPIAsync(string race)
+    //    {
+    //        HttpResponseMessage response = await _httpClient.GetAsync(_baseUrl + _races + race);
+    //        if (response.IsSuccessStatusCode)
+    //        {
+    //            Race_Short result = await response.Content.ReadAsAsync<Race_Short>();
+    //            return result;
+    //        }
+    //        return null;
+    //    }
 
-        public async Task<Classes_Short> GetShortClassFromAPIAsync(string classes)
-        {
-            HttpResponseMessage response = await _httpClient.GetAsync(_baseUrl + _classes + classes);
-            if (response.IsSuccessStatusCode)
-            {
-                Classes_Short result = await response.Content.ReadAsAsync<Classes_Short>();
-                return result;
-            }
-            return null;
-        }
+    //    public async Task<Classes_Short> GetShortClassFromAPIAsync(string classes)
+    //    {
+    //        HttpResponseMessage response = await _httpClient.GetAsync(_baseUrl + _classes + classes);
+    //        if (response.IsSuccessStatusCode)
+    //        {
+    //            Classes_Short result = await response.Content.ReadAsAsync<Classes_Short>();
+    //            return result;
+    //        }
+    //        return null;
+    //    }
     }
 }
